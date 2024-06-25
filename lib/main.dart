da8e0d39fb98dfb97c71bb54bcf7fabf11696c36
Eng.Ali Ben Jahlan <alibenjhlan@gmail.com>
2024-06-25 22:59:32 +0300

    first commit develop

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
+import 'package:flutter/material.dart';
+
 /*
 ╔═══════════════════════════════════════════════════╗
 ║ Created by Ali Ben Jahlan on 06/2024           ║
 ║═══════════════════════════════════════════════════║
 ║ +967 775 892 126                                  ║
-║ alibenjhlan@gmail.com                             ║
-║ Mobile Developer Challenge                        ║
-║ Ecommerce App                                  ║
+║ alibenjhlan@gmail.com                          ║
 ╚═══════════════════════════════════════════════════╝
 */
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
