ecc118bd9ebf1ccdb1e4ee42d8af5a2a684fa172
Eng.Ali Ben Jahlan <alibenjhlan@gmail.com>
2024-06-28 09:17:18 +0300

    update (selcted category background color and default category selcted)

--- lib/feature/home/presentation/widgets/categories_widgets.dart
+++ lib/feature/home/presentation/widgets/categories_widgets.dart
@@ -4,35 +4,39 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../provider/provider.dart';
 
 class CategoriesWidget extends ConsumerWidget {
-  const CategoriesWidget({super.key});
-
+  CategoriesWidget({super.key});
+  bool isSelected = false;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final selectedCategory = ref.watch(selectedQueryProvider.notifier).state;
     final selectedQueryNotifier = ref.read(selectedQueryProvider.notifier);
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: List.generate(
-          categry.length,
-          (index) => InkWell(
-            onTap: () {
-              String cat=categry[index]=="All"?'':categry[index];
-              return selectedQueryNotifier.setQuery(cat);
-            },
-            child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                decoration: BoxDecoration(
-                    color: AppColors.white ,
-                    borderRadius: BorderRadius.circular(20)),
-                child: Text(
-                  categry[index],
-                  style: TextStyles.xLargeBold,
-                )),
-          ),
-        ),
-      ),
+
+    return SizedBox(
+      height: 40,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categry.length,
+          itemBuilder: (context, index) {
+            final isSelected = (selectedCategory == categry[index]) ||
+                (selectedCategory.isEmpty && index == 0);
+            return GestureDetector(
+              onTap: () {
+                selectedQueryNotifier.setQuery(categry[index]);
+              },
+              child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  decoration: BoxDecoration(
+                      color: isSelected ? primaryColor : AppColors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Text(
+                    categry[index],
+                    style: TextStyles.xLargeBold.copyWith(
+                        color: isSelected ? AppColors.white : primaryColor),
+                  )),
+            );
+          }),
     );
   }
 }
